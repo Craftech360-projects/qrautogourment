2ce1644842913bb762ff9b28fb632d6a88d3299a
sanjay <94178708+Nanjunda3@users.noreply.github.com>
2023-10-30 20:37:38 +0530

    mailchanges

--- whatsappSend.js
+++ whatsappSend.js
@@ -1,59 +1,52 @@
 const fs = require('fs');
-const data = require('./data1.json');
 const logger = require('./logger');
 logger.log('This is a log message.');
 
-for (let index = 0; index < 1; index++) {
-  var people= data[index];
+for (let index = 35; index <80; index++) {
+  var detectPhonenumber = require('detect-phonenumber');
+  let data = require('./data1.json');
+  var people = data[index];
   const { name, email, code, phoneNumber } = people;
-
+  if(phoneNumber) {
   const dynamicImageUrl = `https://github.com/Craftech360-projects/qrautogourment/blob/main/images/${code}.png?raw=true`;
 
   var https = require('follow-redirects').https;
 
-var options = {
-  'method': 'POST',
-  'hostname': 'backend.aisensy.com',
-  'path': '/campaign/t1/api',
-  'headers': {
-    'Content-Type': 'application/json'
-  },
-  'maxRedirects': 20
-};
+  // const axios = require('axios');
+  // let data1 = {
+  //   apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1M2IwZDMwMTE5N2FjNDY0YzA3ZTUwYiIsIm5hbWUiOiJDcmFmdGVjaDM2MCIsImFwcE5hbWUiOiJBaVNlbnN5IiwiY2xpZW50SWQiOiI2NTNiMGQzMDExOTdhYzQ2NGMwN2U1MDUiLCJhY3RpdmVQbGFuIjoiQkFTSUNfTU9OVEhMWSIsImlhdCI6MTY5ODM2ODgxNn0.D4J5c6TI2m8HRpDWTD0bI238BwRvtg4GrGcn28-Wg1U",
+  //   campaignName: "Gourmet Luxe 2023",
+  //   destination:  `${phoneNumber}`,
+  //   userName: name,
+  //   templateParams: [
+  //     name
+  //   ],
+  //   media: {
+  //     url: dynamicImageUrl,
+  //     filename: "demo-file"
+  //   }
+  // };
 
-var req = https.request(options, function (res) {
-  var chunks = [];
+  // let config = {
+  //   method: 'post',
+  //   maxBodyLength: Infinity,
+  //   url: 'https://backend.aisensy.com/campaign/t1/api',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   data: data1
+  // };
 
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
 
-  res.on("end", function (chunk) {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
+  // console.log(config);
+  // axios.request(config)
+  //   .then((response) => {
+  //     logger.log(JSON.stringify(response.data));
+  //     console.log(JSON.stringify(response.data));
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 
-  res.on("error", function (error) {
-    console.error(error);
-  });
-});
-
-var postData = JSON.stringify({
-  "apiKey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1M2IwZDMwMTE5N2FjNDY0YzA3ZTUwYiIsIm5hbWUiOiJDcmFmdGVjaDM2MCIsImFwcE5hbWUiOiJBaVNlbnN5IiwiY2xpZW50SWQiOiI2NTNiMGQzMDExOTdhYzQ2NGMwN2U1MDUiLCJhY3RpdmVQbGFuIjoiQkFTSUNfTU9OVEhMWSIsImlhdCI6MTY5ODM2ODgxNn0.D4J5c6TI2m8HRpDWTD0bI238BwRvtg4GrGcn28-Wg1U",
-  "campaignName": "Gourmet Luxe 2023",
-  "destination": phoneNumber,
-  "userName": name,
-  "templateParams": [
-    name
-  ],
-  "media": {
-    "url": dynamicImageUrl,
-    "filename": "demo-file"
-  }
-});
-
-req.write(postData);
-
-req.end();
-
-};
+  };
+}
