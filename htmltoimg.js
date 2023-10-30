8336097c41d7f47bf7c04408785668300888e189
sanjay <94178708+Nanjunda3@users.noreply.github.com>
2023-10-30 12:29:19 +0530

    30oct

--- htmltoimg.js
+++ htmltoimg.js
@@ -14,11 +14,11 @@ if (!fs.existsSync(qrcodeDirectory)) {
 
 let codeNumber = 0;
 let peopleCollection = []
-console.log("people collection is", peopleCollection);
+
 
 const generateImages = async () => {
   for (let index = 0; index < data.length; index++) {
-    let item= data[index];
+    let item = data[index];
     const { name, phoneNumber, email } = item;
     codeNumber++;
     const code = `GL23EF${codeNumber.toString().padStart(4, '0')}`;
@@ -128,10 +128,139 @@ const generateImages = async () => {
 generateImages().then(() => {
   console.log("All images generated successfully");
   console.log("Final people collection:", peopleCollection);
-  peopleCollection.forEach(person=>{
-    //send mail 
+  var imageString = `https://raw.githubusercontent.com/Craftech360-projects/qrautogourment/main/images/{{code}}.png?raw=true`;
 
 
+  //   var htmlString=
+  //   `<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title>GOURMETLUXE</title><!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><style type="text/css">#outlook a { padding:0; }
+  //   body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
+  //   table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; }
+  //   img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; }
+  //   p { display:block;margin:13px 0; }</style><!--[if mso]>
+  // <noscript>
+  // <xml>
+  // <o:OfficeDocumentSettings>
+  //   <o:AllowPNG/>
+  //   <o:PixelsPerInch>96</o:PixelsPerInch>
+  // </o:OfficeDocumentSettings>
+  // </xml>
+  // </noscript>
+  // <![endif]--><!--[if lte mso 11]>
+  // <style type="text/css">
+  //   .mj-outlook-group-fix { width:100% !important; }
+  // </style>
+  // <![endif]--><style type="text/css">@media only screen and (min-width:480px) {
+  //     .mj-column-per-100 { width:100% !important; max-width: 100%; }
+  //   }</style><style media="screen and (min-width:480px)">.moz-text-html .mj-column-per-100 { width:100% !important; max-width: 100%; }</style><style type="text/css">@media only screen and (max-width:479px) {
+  //   table.mj-full-width-mobile { width: 100% !important; }
+  //   td.mj-full-width-mobile { width: auto !important; }
+  // }</style><style type="text/css"></style></head><body style="word-spacing:normal;"><div><!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]--><div style="margin:0px auto;max-width:600px;"><table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"><tbody><tr><td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;"><!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]--><div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"><tbody><tr><td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;"><tbody><tr><td style="width:500px;"><img alt="Full Screen Image" src="https://github.com/Craftech360-projects/qrautogourment/blob/main/images/GL23EF0001.png?raw=true" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="500" height="auto"></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></div></body></html>`
+  const Mailjet = require('node-mailjet');
+  const mailjet = Mailjet.apiConnect(
+    'f8c199da7a532fcb35fc83fce0e9ec55',
+    '60d75ea08bc4ff13fa68f30b531d6942',
+  );
+
+
+  // const request = mailjet
+  //         .post('send', { version: 'v3.1' })
+  //         .request({
+  //           Messages: [
+  //             {
+  //               From: {
+  //                 Email: "abilash@craftech360.com",
+  //                 Name: "abilash"
+  //               },
+  //               To: [
+  //                 {
+  //                   Email: "sanjay@craftech360.com",
+  //                   Name: "passenger 1"
+  //                 }
+  //               ],
+  //               Subject: "Your email flight plan!",
+  //               HTMLPart: htmlString,
+
+  //             }
+  //           ]
+  //         })
+
+  // request
+  //     .then((result) => {
+  //         console.log(result.body)
+  //     })
+  //     .catch((err) => {
+  //         console.log(err.statusCode)
+  //     })
+
+
+  peopleCollection.forEach(async (person) => {
+
+    const { name, email, code } = person;
+    const dynamicImageUrl = `https://github.com/Craftech360-projects/qrautogourment/blob/main/images/${code}.png?raw=true`;
+    var htmlString =
+      `<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title>GOURMETLUXE</title><!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><style type="text/css">#outlook a { padding:0; }
+    body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
+    table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; }
+    img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; }
+    p { display:block;margin:13px 0; }</style><!--[if mso]>
+  <noscript>
+  <xml>
+  <o:OfficeDocumentSettings>
+    <o:AllowPNG/>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+  </o:OfficeDocumentSettings>
+  </xml>
+  </noscript>
+  <![endif]--><!--[if lte mso 11]>
+  <style type="text/css">
+    .mj-outlook-group-fix { width:100% !important; }
+  </style>
+  <![endif]--><style type="text/css">@media only screen and (min-width:480px) {
+      .mj-column-per-100 { width:100% !important; max-width: 100%; }
+    }</style><style media="screen and (min-width:480px)">.moz-text-html .mj-column-per-100 { width:100% !important; max-width: 100%; }</style><style type="text/css">@media only screen and (max-width:479px) {
+    table.mj-full-width-mobile { width: 100% !important; }
+    td.mj-full-width-mobile { width: auto !important; }
+  }</style><style type="text/css"></style></head><body style="word-spacing:normal;"><div><!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]--><div style="margin:0px auto;max-width:600px;"><table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"><tbody><tr><td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;"><!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]--><div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"><tbody><tr><td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;"><tbody><tr><td style="width:500px;"><img alt="Full Screen Image" src="https://github.com/Craftech360-projects/qrautogourment/blob/main/images/${code}.png?raw=true" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="500" height="auto"></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></div></body></html>`
+
+
+    async function sendMail() {
+      console.log(`logging at async ${String(code)}, ${String(name)}, ${String(email)}`);
+
+      var result = mailjet
+        .post('send', { version: 'v3.1' })
+        .request({
+          Messages: [
+            {
+              From: {
+                Email: "abilash@craftech360.com",
+                Name: "abilash"
+              },
+              To: [
+                {
+                  Email: `${email}`,
+                  Name: `${name}`
+                }
+              ],
+              Subject: "Gourmet LUXE 2023 new mail",
+              HTMLPart: htmlString,
+
+            }
+          ]
+        });
+      return result;
+    }
+
+    try {
+      await sendMail().then((result) => {
+        console.log(`Email sent successfully to ${String(name)} (${String(email)}): ${result.body}`);
+      })
+        .catch(err => {
+          console.log(err);
+        });
+
+    } catch (err) {
+      console.error(`Error sending email to ${name} (${email}): ${err.statusCode}`);
+    }
     // send whatsapp
   })
 });
